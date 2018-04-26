87648a49a22ec680e14c1597d96acbd2725edd5d
Sonia  Nicas <sonia.d.nicas@gmail.com>
2018-04-26 02:10:16 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,8 +14,10 @@ require 'pry'
   #   }
   # }
 
-  contacts.each do |person, contact_details_hash|
-    contact_details_hash.each do |attribute, data|
-      binding.pry
+  def remove_strawberry(contacts)
+    contacts.each do |person, contact_details_hash|
+      contact_details_hash.each do |attribute, data|
+        binding.pry
+      end
     end
-  end
+end 
